3d9741bd0464b3387116a382af49a78a5633ce19
henriques200 <65161488+henriques200@users.noreply.github.com>
2022-12-14 16:41:50 +0000

    SQL functionality added

---
+++ sql/init.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE EQUIP_BD;
+
+USE EQUIP_BD;
+
+CREATE TABLE EQUIPAMENTO (
+    Nome VARCHAR(255) NOT NULL,
+    Ip_Nome VARCHAR(255) NOT NULL,
+    Username VARCHAR(255) NOT NULL,
+    Pass VARCHAR(255),
+    OS VARCHAR(255) NOT NULL,
+    Grupo VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Nome)
+);
+
+CREATE TABLE GRUPO(
+    Nome VARCHAR(255) NOT NULL,
+    Dono VARCHAR(255) NOT NULL,
+    Phone INT NOT NULL,
+    NIF INT NOT NULL,
+    PRIMARY KEY(NIF)
+);
+
+CREATE TABLE COMANDO(
+    Nome_codigo VARCHAR(255) NOT NULL,
+    Comando VARCHAR(255) NOT NULL,
+    Descricao VARCHAR(255),
+    OS VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Nome_codigo)
+);
+
+CREATE TABLE SISTEMA(
+    Nome VARCHAR(255) NOT NULL,
+    Fabricante VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Nome)
+);
+
+INSERT INTO SISTEMA (Nome, Fabricante)
+VALUES
+    ('Windows10', 'Microsoft'),
+    ('Ubuntu20.04', "Canonical"),
+    ('RouterOS', 'Microtik'),
+    ('SwitchOS', 'Microtik'),
+    ('Cisco IOS', 'Cisco');
