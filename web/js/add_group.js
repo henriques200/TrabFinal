68cca4d80eecb58e54b515dc26abbd35a579fe5c
henriques200 <65161488+henriques200@users.noreply.github.com>
2022-12-21 15:12:51 +0000

    Update group submission files.

--- web/js/add_group.js
+++ web/js/add_group.js
@@ -1,20 +1,55 @@
 $(function(){
-    $("#submit_group").click(function(){
+    $("form").submit(function(e){
+        /*
+        Check if post content is not empty.
+        */
         var group_name = $("#nome_group").val().toString();
         var group_owner = $("#dono_group").val().toString();
-        check_output(group_name, group_owner);
+        var phone_number = $("#phone_number").val().toString();
+        var nif = $("#nif").val().toString();
+        if(group_name.length === 0) {
+            alert("Nome Vazio!");
+            e.preventDefault();
+            return false;
+        } else if(group_owner.length === 0) {
+            alert("Introduz o nome do dono!");
+            e.preventDefault();
+            return false;
+        } else if(phone_number.length === 0) {
+            alert("Introduz o nº de telefone!");
+            e.preventDefault();
+            return false;
+        } else if(nif.length === 0) {
+            alert("Introduz o NIF!");
+            e.preventDefault();
+            return false;
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: './submit_group.php',
+                data: $('#submit_new_group').serializeArray(),
+                dataType: "json",
+                encode: true,
+                cache: false,
+                beforeSend: function () {
+                    //We add this before send to disable the button once we submit it so that we prevent the multiple click
+                    $('#submit_group').attr("disabled", true);
+                },
+                success: function(data){
+                    console.log(data); //Para fins de diagnóstico.
+                    if(data.error === 1) window.alert ("Erro! O nome do grupo já existe!");
+                    else window.alert("Dados submetidos!");
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    window.alert("Erro nos dados submetidos!");
+                },
+                complete: function(){
+                    $("#submit_new_group").trigger("reset");
+                    $("#submit_group").attr("disabled", false);
+                }
+            });
+            e.preventDefault();
+            return false;
+        }
     });
 });
-
-function check_output(name, owner){
-    /*
-    Function to check the HTML form output.
-    */
-    if(name.length === 0) {
-        console.log("Nome Vazio!");
-        window.alert("Nome Vazio!");
-    }else if(owner.length === 0){
-        console.log("Introduz o nome do dono!");
-        window.alert("Introduz o nome do dono!");
-    }
-}
--- web/submit_group.php
+++ web/submit_group.php
@@ -1,8 +1,60 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "EQUIP_BD";
+$error = 0;
+$msg = "";
+$redirect = "";
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $group_name = $_REQUEST['nome_group'];
-    $group_owner = $_REQUEST['dono_group'];
-    echo "<p>Nome Grupo: " . $group_name;
-    echo "<p>Dono: " . $group_owner;
+  //Fetch the required form data.
+  $group_name = $_REQUEST['nome_group'];
+  $group_owner = $_REQUEST['dono_group'];
+  $phone_number = $_REQUEST['phone_number'];
+  $nif = $_REQUEST['nif'];
+
+  //Verifiy if values are empty.
+  if(empty($group_name) || empty($group_owner) || empty($phone_number) || empty($nif)){
+    $error = 1;
+    $msg = "Os valores introduzidos estão incompletos!";
+  } else {
+    try {
+
+      //Create connection.
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      //Set the PDO error mode to exception.
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+      //Query to check certain groupname.
+      $search_group_name = $conn->query("SELECT COUNT(*) FROM GRUPO WHERE Nome='$group_name'");
+
+      //Check if Group Name already exists.
+      if($search_group_name->fetchColumn() > 0){
+        $error = 1;
+        $msg = "O nome do grupo $group_name já existe!";
+      } else {
+        //Insert new record.
+        $sql_insert_query = "INSERT INTO GRUPO (Nome, Dono, Phone, NIF) VALUES ('$group_name', '$group_owner', '$phone_number', '$nif')";
+        $new_record = $conn->exec($sql_insert_query);
+        $msg = "O grupo $group_name foi adicionado com sucesso!";
+      }
+
+    } catch(PDOException $conn_error) {
+      $error = 1;
+      $msg = "Connection failed: " . $conn_error->getMessage();
+    }
+    
+    //Closes DB connection.
+    $conn = null;
+  }
+} else {
+  $error = 1;
+  $msg = "Método HTML incorreto!";
 }
+
+
+//Envia a resposta para a página HTML com o AJAX.
+echo json_encode(array('error' => $error, 'message' => $msg, 'redirect' => $redirect));
 ?>
