a3a62f8814b97991d2b62f6333b5fc579cb50780
Emanuel Henriques <emanuel.h200@hotmail.com>
2023-01-14 10:20:08 +0000

    update env.php

--- web/env.php
+++ web/env.php
@@ -1,21 +1,17 @@
 <?php
-$servername = getenv("SRV_ADDR");
-$username = getenv("WEB_USER");
-$password = getenv("WEB_PASSWORD");
+//DB access parameters
+$servername = getenv("DB_ADDR");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 $dbname = "EQUIP_BD";
+
+//PHP AJAX comunication options
 $error = 0;
 $msg = "";
 $redirect = "";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-/*
-if(is_null($servername)) $servername = "localhost";
-if(is_null($username)) $username = "root";
-if(is_null($password)) $password = "";
-if(is_null($dbname)) $dbname = "EQUIP_BD";
-*/
+//Check DB access parameters
+if(empty($servername)) $servername = "localhost";
+if(empty($username)) $username = "root";
 
 ?>
