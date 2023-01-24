cd4503c6b42fa09576c438c4aeac5dea2f83c58f
Emanuel Henriques <emanuel.h200@hotmail.com>
2023-01-24 13:24:42 +0000

    Update session feature.

--- web/env.php
+++ web/env.php
@@ -7,6 +7,8 @@ $servername = getenv("DB_ADDR");
 $username = getenv("DB_USER");
 $password = getenv("DB_PASSWORD");
 $dbname = "EQUIP_BD";
+$web_user = getenv("WEB_USER");
+$web_pass = getenv("WEB_PASSWORD");
 
 //PHP AJAX comunication options
 $error = 0;
@@ -19,6 +21,10 @@ $timezone = getenv("TIMEZONE");
 //Check DB access parameters
 if(empty($servername)) $servername = "localhost";
 if(empty($username)) $username = "root";
+
+//Check Web access parameters
+if(empty($web_user)) $web_user = "root";
+if(empty($web_pass)) $web_pass = "root";
 
 //Timezone parameters
 if (empty($timezone)) date_default_timezone_set("Europe/Lisbon");
--- web/login.php
+++ web/login.php
@@ -2,24 +2,25 @@
 require("env.php");
 require("events.php");
 
-$user = "root";
-$pass = "12345";
-
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if($_POST['username'] == $user && $_POST['password'] = $pass){
+    if($_POST['username'] == $web_user && $_POST['password'] = $web_pass){
         session_start();
-        $_SESSION['username'] = $user;
+        $_SESSION['username'] = $web_user;
+        $_SESSION['password'] = $web_pass;
         $_SESSION["loggedin"] = true;
 
         // Redirect user to welcome page
-        //header("location: welcome.php");
+        header("location: index.html");
     } else {
+        unset($_SESSION['username']);
+        unset($_SESSION['password']);
         $error = 1;
         $msg = "Credenciais Inválidas!";
+        new_event("ERRO", "Tentativa de acesso indevido!");
+        header("location: login.html");
     }
 } else {
     $error = 1;
     $msg = "Método inválido!";
 }
-
 ?>
